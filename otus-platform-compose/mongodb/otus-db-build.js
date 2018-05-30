343fd5146ae69b61f2f81da05ecfb307c26f643f
Tiago Matana <tiago.matana@gmail.com>
2018-05-30 14:45:07 -0300

    editorconfig added

--- otus-platform-compose/mongodb/otus-db-build.js
+++ otus-platform-compose/mongodb/otus-db-build.js
@@ -1,27 +1,30 @@
 db = db.getSiblingDB('admin');
-db.createUser(
-    {
-        user: "admin",
-        pwd: "XRYs9yjU",
-        roles: [ { role: "root", db: "admin" } ]
-    }
-);
-db.auth("admin","XRYs9yjU");
+db.createUser({
+  user: "admin",
+  pwd: "XRYs9yjU",
+  roles: [{
+    role: "root",
+    db: "admin"
+  }]
+});
+db.auth("admin", "XRYs9yjU");
 
 db = db.getSiblingDB('otus');
-db.createUser(
-    {
-        user: "otus",
-        pwd: "a9J1sVtL",
-        roles: [ { role: "dbOwner", db: "otus" } ]
-    }
-);
+db.createUser({
+  user: "otus",
+  pwd: "a9J1sVtL",
+  roles: [{
+    role: "dbOwner",
+    db: "otus"
+  }]
+});
 
 db = db.getSiblingDB('otus-domain');
-db.createUser(
-    {
-        user: "otus",
-        pwd: "fLACLaFL",
-        roles: [ { role: "dbOwner", db: "otus-domain" } ]
-    }
-);
+db.createUser({
+  user: "otus",
+  pwd: "fLACLaFL",
+  roles: [{
+    role: "dbOwner",
+    db: "otus-domain"
+  }]
+});
